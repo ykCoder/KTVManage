49d3c33a1feffbba17ef65941d71ef4af8a1f347
杨康 <2842928569@qq.com>
2020-11-04 15:02:01 +0800

    初始化项目

---
+++ KTVManage/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  KTVManage
+//
+//  Created by 杨康 on 2020/11/4.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RootViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
